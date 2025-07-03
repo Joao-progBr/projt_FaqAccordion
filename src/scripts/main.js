8b2c0ed849f8ebe3fbb72bb84a2f82f92972c218
joãoA <joaovictor.dudu03@gmail.com>
2025-07-02 21:50:32 -0300

    melhoria da faq. adicionado responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,30 @@
 const faqQuestion = document.querySelectorAll('.faq__question')
-const faqAnswer = document.querySelectorAll('.faq__answer')
 
 faqQuestion.forEach(function(btn){
+    // console.log(btn)
+    const icon = btn.querySelector('.faq-icon')
+    // console.log(icon)
     btn.addEventListener('click', function(){
         const faqAnswer = btn.nextElementSibling
-        faqAnswer.classList.toggle('active')
+        const isOPen = faqAnswer.classList.contains('active')
+
+        document.querySelectorAll('.faq__answer').forEach(answer => {
+            answer.classList.remove('active')
+            // console.log(answer)
+        })
+
+        document.querySelectorAll('.faq-icon').forEach(icon => {
+            icon.src = './build/assets/images/icon-plus.svg'
+            // console.log(icon)
+        })
+
+        if(!isOPen){
+            faqAnswer.classList.add('active')
+            icon.src = './build/assets/images/icon-minus.svg'
+        }
+        // faqAnswer.classList.toggle('active')
+        // const isOpen = faqAnswer.classList.contains('active')
+        // icon.src = isOpen ? './build/assets/images/icon-minus.svg' : './build/assets/images/icon-plus.svg'
     })
 })
+
